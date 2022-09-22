c1dbd0ad4c8fdba6b0e5d3cdcd51d50af6d8b71b
DanielFalcony <DanielFalcony@gmail.com>
2022-09-22 09:56:34 +0300

    Rebuild v4 Ready button without "/"

--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -1,20 +1,20 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 
 # Кнопки навигации в боте
-b1 = KeyboardButton('/показать_весь_список_контактов')
-b2 = KeyboardButton('/добавить_контакт')
-b3 = KeyboardButton('/удалить_контакт')
-b4 = KeyboardButton('/редактировать_контакт')
-b5 = KeyboardButton('/найти_контакт')
-b6 = KeyboardButton('/импорт_контактов')
-b7 = KeyboardButton('/экспорт_контактов')
-b8 = KeyboardButton('/help')
+b1 = KeyboardButton('Показать весь список контактов')
+b2 = KeyboardButton('Добавить контакт')
+b3 = KeyboardButton('Удалить контакт')
+b4 = KeyboardButton('Редактировать контакт')
+b5 = KeyboardButton('Найти контакт')
+b6 = KeyboardButton('Импорт контактов')
+b7 = KeyboardButton('Экспорт контактов')
+b8 = KeyboardButton('Справка')
 b9 = KeyboardButton('Поделиться номером', request_contact=True)
 
 
 
 # Создание класса замещения клавиатуры пользователя на кнопки навигации в боте
-kb_client = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+kb_client = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
 
 # Добавляем кнопки в клавиатуру
 kb_client.add(b1).add(b2).insert(b3).add(b4).insert(b5).add(b6).insert(b7).add(b8).add(b9)
